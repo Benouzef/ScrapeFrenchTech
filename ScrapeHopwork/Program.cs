3eaac43305b2c433c1b7b250538d81f1c60d4561
Benouzef <benoit.fillon@gmail.com>
2017-03-19 10:13:21 +0100

    Création du projet pour scraper une autre source

---
+++ ScrapeHopwork/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeHopwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            WebClient wc = new WebClient();
+            wc.Encoding = Encoding.UTF8;
+
+
+            doc.LoadHtml(wc.DownloadString("http://lespepitestech.com/french-tech-hub/paris-le-de-france"));
+        }
+    }
+}
